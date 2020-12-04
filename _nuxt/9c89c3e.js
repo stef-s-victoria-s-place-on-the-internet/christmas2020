03d5c7e07cb7c76076050f8a9a512a276ace5567
StefKors <stef.kors@gmail.com>
2020-12-04 20:30:08 +0000

    Deploying to gh-pages from master @ 0939491af42220e09a7777465bac8d5987ea6ddd 🚀

---
+++ _nuxt/9c89c3e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{512:function(t,e,n){"use strict";n.r(e);var c={},l=n(41),component=Object(l.a)(c,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("shop catalog")])])}],!1,null,null,null);e.default=component.exports}}]);
